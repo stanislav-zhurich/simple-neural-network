07e42faf49c20455904b758d5922365826c4aeed
Stanislav Zhurich <stanislav_zhurich@epam.com>
2024-02-05 17:01:20 +0300

    Added mini-batch support

--- simplenet/SimpleNet.py
+++ simplenet/SimpleNet.py
@@ -34,7 +34,7 @@ class SimpleNet:
             for j in range(X_test.shape[1]):
                 X_j = np.expand_dims(X_test[:, j], axis = 1)
                 Y_j = np.expand_dims(Y_test[:, j], axis = 1)
-                AL, caches = self._forward_propagation(X_j, parameters)
+                AL, caches = self.__forward_propagation(X_j, parameters)
                 gradients = self.__back_propagation(Y_j, AL, caches)
                 parameters = self.__update_params(parameters, gradients)
                 cost_function = CostFunction.lookup_cost_function(cost_function_str)
@@ -44,27 +44,52 @@ class SimpleNet:
                 print(i, cost)
         self.parameters = parameters
         return parameters
-
+    
+    def __train_mini_batch(self, batched_X, batched_Y, parameters, layers):
+        cost_function_str = self.configuration["cost_function"]
+        AL, caches = self.__forward_propagation(batched_X, parameters, layers)
+        gradients = self.__back_propagation(batched_Y, AL, caches, layers)
+        parameters = self.__update_params(parameters, gradients, layers)
+        cost_function = CostFunction.lookup_cost_function(cost_function_str)
+        cost = cost_function.calculate(batched_Y, AL, self.layers, parameters, self.configuration)
+        return parameters, cost
+    
     def train(self, X_test: np.array, Y_test: np.array):
         epoch = self.configuration["epoch"]
-        cost_function_str = self.configuration["cost_function"]
-        self.layers[:0] = [Layer(X_test.shape[0])]
+        batch_size = self.configuration["batch_size"]
+        batch_iterations = int(X_test.shape[1]/batch_size)
+        remains = X_test.shape[1] - batch_size * batch_iterations
+        layers = copy.deepcopy(self.layers)
+        layers[:0] = [Layer(X_test.shape[0])]
 
-        parameters = self.__initialize_parameters()
+        parameters = self.__initialize_parameters(layers)
+        costs = []
+
         for i in range(0, epoch):
-            AL, caches = self._forward_propagation(X_test, parameters)
-            gradients = self.__back_propagation(Y_test, AL, caches)
-            parameters = self.__update_params(parameters, gradients)
-            cost_function = CostFunction.lookup_cost_function(cost_function_str)
-            cost = cost_function.calculate(Y_test, AL, self.layers, parameters, self.configuration)
+            cost_sum = 0
+            shuffled_X, shuffled_Y = self.__shuffle_dataset(X_test, Y_test)
+            for j in range(batch_iterations):
+                batched_X = shuffled_X[:, batch_size * j: batch_size * j + batch_size ]
+                batched_Y = shuffled_Y[:, batch_size * j: batch_size * j + batch_size ]
+                parameters, cost = self.__train_mini_batch(batched_X, batched_Y, parameters, layers)
+                cost_sum += cost
+            if remains > 0:
+                batched_X = shuffled_X[:, -remains: ]
+                batched_Y = shuffled_Y[:, -remains: ]
+                parameters, cost = self.__train_mini_batch(batched_X, batched_Y, parameters, layers)
+                cost_sum += cost
+            cost = cost_sum/X_test.shape[1]
+            costs.append(np.squeeze(cost).item())
             if i % 100 == 0:
                 print(i, cost)
-            #print(AL)
-        self.parameters = parameters
-        return parameters
+            self.parameters = parameters
+        return costs
+
     
     def predict(self, X):
         layers = self.layers
+        layers = copy.deepcopy(self.layers)
+        layers[:0] = [Layer(X.shape[0])]
         A_prev = X
         for i in range(1, len(layers)):
             W = self.parameters["W" + str(i)]
@@ -75,24 +100,22 @@ class SimpleNet:
             A_prev = A
         return probs.astype(int)
     
-    def __initialize_parameters(self):
+    def __initialize_parameters(self, layers):
         parameters = {}
-        layers = self.layers
         for i in range(1, len(layers)):
             parameters["W" + str(i)] = np.random.randn(layers[i].node_num, layers[i - 1].node_num) * np.sqrt(2/layers[i-1].node_num) 
             parameters["b" + str(i)] = np.zeros((layers[i].node_num, 1)) 
         return parameters
     
-    def __update_params(self, parameters, gradients):
+    def __update_params(self, parameters, gradients, layers):
         learning_rate = self.configuration["learning_rate"]
-        for i in range(1, len(self.layers)):
+        for i in range(1, len(layers)):
             parameters["W" + str(i)] = parameters["W" + str(i)] - learning_rate * gradients["dW" + str(i)]
             parameters["b" + str(i)] = parameters["b" + str(i)] - learning_rate * gradients["db" + str(i)]
         return parameters
     
-    def __back_propagation(self, Y, AL, caches):
+    def __back_propagation(self, Y, AL, caches, layers):
 
-        layers = self.layers
         keep_prob = self.configuration["keep_prob"]
         lambd = self.configuration["lambda"]
 
@@ -123,9 +146,8 @@ class SimpleNet:
         return gradients
     
     
-    def _forward_propagation(self, X_test, parameters):
+    def __forward_propagation(self, X_test, parameters, layers):
         keep_prob = self.configuration["keep_prob"]
-        layers = self.layers
         A_prev = X_test
         dim_len = len(layers)
         caches = []
@@ -133,7 +155,6 @@ class SimpleNet:
             W = parameters["W" + (str(i))]
             b = parameters["b" + (str(i))]
             Z = np.dot(W, A_prev) + b
-
             activation = layers[i].activation
 
             if activation is not None:
@@ -147,3 +168,9 @@ class SimpleNet:
                 caches.append((A_prev, W, b, Z, dropout_matrix))
             A_prev = A.copy()
         return A_prev, caches
+    
+    def __shuffle_dataset(self, X, Y):
+        permutation = list(np.random.permutation(X.shape[1]))
+        shuffled_X = X[:, permutation]
+        shuffled_Y = Y[:, permutation]
+        return shuffled_X, shuffled_Y
